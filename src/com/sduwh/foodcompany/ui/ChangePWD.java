c49ec44ba3796244508c6a4cf129f4bf4c8a0743
dongzhe <ncxxdz@126.com>
2018-07-21 18:20:43 +0800

    董喆界面

--- src/com/sduwh/foodcompany/ui/ChangePWD.java
+++ src/com/sduwh/foodcompany/ui/ChangePWD.java
@@ -2,54 +2,88 @@ package com.sduwh.foodcompany.ui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+
 
 public class ChangePWD extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField_id_c;
+	private JTextField textField_old_pass_c;
+	private JTextField textField_new_pass_c;
+	private JTextField textField_new_con_pass_c;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			ChangePWD dialog = new ChangePWD();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+
+
 
 	/**
 	 * Create the dialog.
 	 */
 	public ChangePWD() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 480, 400);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+		contentPanel.setLayout(null);
+		
+		JLabel label_c_id = new JLabel("\u7528\u6237\u540D\uFF1A");
+		label_c_id.setFont(new Font("宋体", Font.PLAIN, 14));
+		label_c_id.setBounds(91, 51, 66, 28);
+		contentPanel.add(label_c_id);
+		
+		textField_id_c = new JTextField();
+		textField_id_c.setBounds(166, 52, 169, 28);
+		contentPanel.add(textField_id_c);
+		textField_id_c.setColumns(10);
+		
+		JLabel label_old_pass = new JLabel("\u65E7\u5BC6\u7801\uFF1A");
+		label_old_pass.setFont(new Font("宋体", Font.PLAIN, 14));
+		label_old_pass.setBounds(91, 107, 77, 16);
+		contentPanel.add(label_old_pass);
+		
+		textField_old_pass_c = new JTextField();
+		textField_old_pass_c.setColumns(10);
+		textField_old_pass_c.setBounds(166, 102, 169, 28);
+		contentPanel.add(textField_old_pass_c);
+		
+		JLabel label_new_pass = new JLabel("\u65E7\u5BC6\u7801\uFF1A");
+		label_new_pass.setFont(new Font("宋体", Font.PLAIN, 14));
+		label_new_pass.setBounds(91, 154, 77, 16);
+		contentPanel.add(label_new_pass);
+		
+		JLabel label__new_con_pass_c = new JLabel("\u786E\u8BA4\u5BC6\u7801\uFF1A");
+		label__new_con_pass_c.setFont(new Font("宋体", Font.PLAIN, 14));
+		label__new_con_pass_c.setBounds(79, 200, 77, 16);
+		contentPanel.add(label__new_con_pass_c);
+		
+		textField_new_pass_c = new JTextField();
+		textField_new_pass_c.setColumns(10);
+		textField_new_pass_c.setBounds(166, 152, 169, 28);
+		contentPanel.add(textField_new_pass_c);
+		
+		textField_new_con_pass_c = new JTextField();
+		textField_new_con_pass_c.setColumns(10);
+		textField_new_con_pass_c.setBounds(166, 195, 169, 28);
+		contentPanel.add(textField_new_con_pass_c);
+		
+		JButton button_c_changpass = new JButton("\u4FEE\u6539");
+		button_c_changpass.setFont(new Font("黑体", Font.BOLD, 15));
+		button_c_changpass.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		});
+		button_c_changpass.setBounds(175, 252, 93, 33);
+		contentPanel.add(button_c_changpass);
 	}
 
 }
--- src/com/sduwh/foodcompany/ui/PersonInfoPanel.java
+++ src/com/sduwh/foodcompany/ui/PersonInfoPanel.java
@@ -13,139 +13,193 @@ import javax.swing.JButton;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
 /*
  * create by dongzhe
  */
-public class PersonInfoPanel extends JPanel {
+public class PersonInfoPanel extends JPanel implements ActionListener{
+	//用户名文本框
 	private JTextField textField_user_id;
+	//用户姓名文本框
 	private JTextField textField_user_name;
+	//电话标签
 	private JLabel label_tel;
+	//电话输入框
 	private JTextField textField_tel;
+	//身份标签
 	private JLabel label__admin_power;
-	private JTextField textField;
-
+	//身份输入框
+	private JTextField textField_admin_power;
+	private JLabel label;
+	private JLabel label_1;
+	//按钮
+	JButton btn_edit;
+	JButton button_save;
 	/**
 	 * Create the panel.
 	 */
 	public PersonInfoPanel() {
 		//初始设置
 		setSize(800,700);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{222, 112, 192, 111, 192, 0};
-		gridBagLayout.rowHeights = new int[]{0, 84, 131, 34, 59, 34, 50, 40, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
-		
+		setLayout(null);
+		/*
+		 * 标签和输入框的样式设置
+		 */
 		JLabel label_user_id = new JLabel("用户名:");
+		label_user_id.setBounds(139, 208, 107, 19);
 		label_user_id.setFont(new Font("宋体", Font.PLAIN, 16));
 		label_user_id.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_label_user_id = new GridBagConstraints();
-		gbc_label_user_id.fill = GridBagConstraints.HORIZONTAL;
-		gbc_label_user_id.insets = new Insets(0, 0, 5, 5);
-		gbc_label_user_id.gridx = 1;
-		gbc_label_user_id.gridy = 3;
-		add(label_user_id, gbc_label_user_id);
+		add(label_user_id);
 		label_user_id.setFont(new Font("宋体", Font.PLAIN, 16));
-		
+		//用户名输入框
 		textField_user_id = new JTextField();
+		textField_user_id.setBounds(257, 203, 146, 29);
 		textField_user_id.setFont(new Font("宋体", Font.PLAIN, 16));
 		textField_user_id.setEditable(false);
 		textField_user_id.setEnabled(false);
-		GridBagConstraints gbc_textField_user_id = new GridBagConstraints();
-		gbc_textField_user_id.fill = GridBagConstraints.BOTH;
-		gbc_textField_user_id.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_user_id.gridx = 2;
-		gbc_textField_user_id.gridy = 3;
-		add(textField_user_id, gbc_textField_user_id);
+		add(textField_user_id);
 		textField_user_id.setColumns(10);
-		
+		//姓名标签
 		JLabel label_user_name = new JLabel("姓名：");
+		label_user_name.setBounds(433, 208, 96, 19);
 		label_user_name.setFont(new Font("宋体", Font.PLAIN, 16));
 		label_user_name.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_label_user_name = new GridBagConstraints();
-		gbc_label_user_name.fill = GridBagConstraints.HORIZONTAL;
-		gbc_label_user_name.insets = new Insets(0, 0, 5, 5);
-		gbc_label_user_name.gridx = 3;
-		gbc_label_user_name.gridy = 3;
-		add(label_user_name, gbc_label_user_name);
-		
+		add(label_user_name);
+		//姓名输入框
 		textField_user_name = new JTextField();
+		textField_user_name.setBounds(539, 203, 179, 29);
 		textField_user_name.setFont(new Font("宋体", Font.PLAIN, 16));
 		textField_user_name.setEnabled(false);
 		textField_user_name.setEditable(false);
 		textField_user_name.setColumns(10);
-		GridBagConstraints gbc_textField_user_name = new GridBagConstraints();
-		gbc_textField_user_name.fill = GridBagConstraints.BOTH;
-		gbc_textField_user_name.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_user_name.gridx = 4;
-		gbc_textField_user_name.gridy = 3;
-		add(textField_user_name, gbc_textField_user_name);
-		
+		add(textField_user_name);
+		//电话标签
 		label_tel = new JLabel("电话：");
+		label_tel.setBounds(152, 305, 107, 19);
 		label_tel.setHorizontalAlignment(SwingConstants.CENTER);
 		label_tel.setFont(new Font("宋体", Font.PLAIN, 16));
-		GridBagConstraints gbc_label_tel = new GridBagConstraints();
-		gbc_label_tel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_label_tel.insets = new Insets(0, 0, 5, 5);
-		gbc_label_tel.gridx = 1;
-		gbc_label_tel.gridy = 5;
-		add(label_tel, gbc_label_tel);
-		
+		add(label_tel);
+		//电话输入框
 		textField_tel = new JTextField();
+		textField_tel.setBounds(257, 300, 146, 29);
 		textField_tel.setFont(new Font("宋体", Font.PLAIN, 16));
 		textField_tel.setEnabled(false);
 		textField_tel.setEditable(false);
 		textField_tel.setColumns(10);
-		GridBagConstraints gbc_textField_tel = new GridBagConstraints();
-		gbc_textField_tel.fill = GridBagConstraints.BOTH;
-		gbc_textField_tel.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_tel.gridx = 2;
-		gbc_textField_tel.gridy = 5;
-		add(textField_tel, gbc_textField_tel);
+		add(textField_tel);
 		
 		label__admin_power = new JLabel("身份：");
+		label__admin_power.setBounds(444, 305, 73, 19);
 		label__admin_power.setHorizontalAlignment(SwingConstants.CENTER);
 		label__admin_power.setFont(new Font("宋体", Font.PLAIN, 16));
-		GridBagConstraints gbc_label__admin_power = new GridBagConstraints();
-		gbc_label__admin_power.fill = GridBagConstraints.HORIZONTAL;
-		gbc_label__admin_power.insets = new Insets(0, 0, 5, 5);
-		gbc_label__admin_power.gridx = 3;
-		gbc_label__admin_power.gridy = 5;
-		add(label__admin_power, gbc_label__admin_power);
+		add(label__admin_power);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("宋体", Font.PLAIN, 16));
-		textField.setEnabled(false);
-		textField.setEditable(false);
-		textField.setColumns(10);
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.fill = GridBagConstraints.BOTH;
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.gridx = 4;
-		gbc_textField.gridy = 5;
-		add(textField, gbc_textField);
+		textField_admin_power = new JTextField();
+		textField_admin_power.setBounds(539, 300, 179, 29);
+		textField_admin_power.setFont(new Font("宋体", Font.PLAIN, 16));
+		textField_admin_power.setEnabled(false);
+		textField_admin_power.setEditable(false);
+		textField_admin_power.setColumns(10);
+		add(textField_admin_power);
+		/*
+		 * 添加按钮
+		 */
+		btn_edit = new JButton("编辑");
+		btn_edit.setFont(new Font("黑体", Font.BOLD, 14));
+		btn_edit.setBounds(285, 388, 107, 40);
+		btn_edit.addActionListener(this);
+		add(btn_edit);
+		button_save = new JButton("保存");
+		button_save.setFont(new Font("黑体", Font.BOLD, 14));
+		button_save.setForeground(Color.BLACK);
+		button_save.setBounds(503, 388, 96, 40);
+		button_save.addActionListener(this);
+		button_save.setEnabled(false);
+		add(button_save);
 		
-		JButton btn_edit = new JButton("\u7F16\u8F91");
-		GridBagConstraints gbc_btn_edit = new GridBagConstraints();
-		gbc_btn_edit.fill = GridBagConstraints.BOTH;
-		gbc_btn_edit.insets = new Insets(0, 0, 0, 5);
-		gbc_btn_edit.gridx = 1;
-		gbc_btn_edit.gridy = 7;
-		add(btn_edit, gbc_btn_edit);
-		
-		JButton button_edit = new JButton("\u4FDD\u5B58");
-		button_edit.setEnabled(false);
-		GridBagConstraints gbc_button_edit = new GridBagConstraints();
-		gbc_button_edit.fill = GridBagConstraints.BOTH;
-		gbc_button_edit.insets = new Insets(0, 0, 0, 5);
-		gbc_button_edit.gridx = 3;
-		gbc_button_edit.gridy = 7;
-		add(button_edit, gbc_button_edit);
-		DefaultTableModel defaultTableModel = new DefaultTableModel(4,2);
+		label = new JLabel("\u5A01 \u6D77 \u98DF \u54C1 \u5382");
+		label.setFont(new Font("隶书", Font.BOLD, 45));
+		label.setBounds(288, 81, 398, 40);
+		add(label);
+		/*
+		 * 添加标签 并添加点击事件
+		 */
+		label_1 = new JLabel("<html><u>修改密码</u></html>");
+		label_1.setFont(new Font("宋体", Font.PLAIN, 17));
+		label_1.setForeground(Color.BLUE);
+		label_1.setBounds(723, 10, 107, 29);
+		add(label_1);
+		//添加鼠标点击事件
+		label_1.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				ChangePWD changePWD = new ChangePWD();
+				Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+				int width = (int)screensize.getWidth();
+			    int height = (int)screensize.getHeight();
+			    changePWD.setLocation(new Point(width*1/4, height*1/5));
+			    changePWD.setAlwaysOnTop(true);
+				changePWD.show();
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String name = e.getActionCommand();
+		if(name.equals("编辑")) {
+			textField_tel.setEnabled(true);
+			textField_tel.setEditable(true);
+			button_save.setEnabled(true);
+			btn_edit.setEnabled(false);		
+			
+		}
+		if(name.equals("保存")) {
+			btn_edit.setEnabled(true);
+			btn_edit.setEnabled(true);
+			button_save.setEnabled(false);
+			textField_tel.setEnabled(false);
+			textField_tel.setEditable(false);
+		}
 		
-		
-		//设置边框
 	}
 }
